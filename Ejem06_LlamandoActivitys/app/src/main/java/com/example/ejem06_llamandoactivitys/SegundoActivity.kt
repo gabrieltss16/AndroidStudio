f9d2373d435f9e6f2dc27d47c139a40832a308d6
Gabriel Tomás Sifuentes <gabriel.tomas.sifuentes@gregoriofer.com>
2025-12-04 18:07:59 +0100

    Add files via upload

---
+++ Ejem06_LlamandoActivitys/app/src/main/java/com/example/ejem06_llamandoactivitys/SegundoActivity.kt
@@ -0,0 +1,41 @@
+package com.example.ejem06_llamandoactivitys
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ejem06_llamandoactivitys.databinding.ActivitySegundoBinding
+
+class SegundoActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySegundoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding= ActivitySegundoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //val datoRecibido= intent.getStringExtra("DATO")
+        val datoRecibido:Usuario= intent.getParcelableExtra("DATO",Usuario::class.java)!!
+        //binding.tvDatoRecibido.text=datoRecibido
+
+
+
+        binding.bDevolver.setOnClickListener {
+            val datoDevolver=binding.tietDatoDevuelto.text.toString()
+            Log.d("depurando","Dato a devolver: $datoDevolver")
+
+            intent.putExtra("dato_devuelto",datoDevolver)
+            setResult(RESULT_OK,intent)
+            finish()
+        }
+
+
+
+
+    }
+}
